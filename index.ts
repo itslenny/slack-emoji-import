d1a1a36cc4753a7fb1b3fb04ec5d4d7817e4196e
itslenny <lenny@itslennysfault.com>
2018-09-11 04:40:21 -0700

    init

---
+++ index.ts
@@ -0,0 +1,183 @@
+import { mkdirSync, createWriteStream, readFile } from 'fs';
+import { resolve, basename } from 'path';
+import { Page } from 'puppeteer';
+import * as puppeteer from 'puppeteer';
+import * as request from 'request';
+import * as yaml from 'js-yaml';
+import * as prompt from 'prompt';
+
+interface Emoji {
+    name: string;
+    src: string;
+}
+
+interface EmojiPack {
+    title: string;
+    emojis: Emoji[];
+}
+
+interface UserInput {
+    host: string;
+    email: string;
+    password: string;
+}
+
+if (process.argv.length < 3) {
+    console.log('usage: slack-emoji-import path/to/emoji-pack.yaml');
+    process.exit(1);
+}
+
+const TEMP_DIR = resolve(__dirname, '.tmp');
+const MIME_TYPES = new Set(['jpg', 'jpeg', 'png', 'gif']);
+const ENTRY_URL_FACTORY = host => `https://${host}.slack.com/?redir=%2Fcustomize%2Femoji`;
+const YAML_PATH = resolve(__dirname, process.argv[2]);
+
+try {
+    mkdirSync(TEMP_DIR);
+} catch (e) { }
+
+start();
+
+/**
+ * 
+ */
+async function start(): Promise<void> {
+    const userInput = await getUserInput();
+
+    const browser = await puppeteer.launch({ headless: false, defaultViewport: { width: 1200, height: 1000 } });
+    const browserCtx = await browser.createIncognitoBrowserContext();
+    const page = await browserCtx.newPage();
+
+    console.log('logging in...')
+    await login(page, userInput);
+    console.log('logged in.');
+
+    const emojiPack = await loadEmojiPack(YAML_PATH);
+    for (let emoji of emojiPack.emojis) {
+        console.log(`downloading ${emoji.name}...`);
+        const imagePath = await downloadImage(emoji.src);
+        console.log(`downloaded  ${emoji.name}.`);
+
+        console.log(`uploading ${emoji.name}...`);
+        await upload(page, imagePath, emoji.name).then(sleep(10));
+        console.log(`uploaded  ${emoji.name}.`);
+    }
+
+    await browser.close();
+}
+
+/**
+ * 
+ */
+function getUserInput(): Promise<UserInput> {
+    return new Promise((promiseResolve, promiseReject) => {
+        prompt.get([
+            {
+                description: 'Slack Host',
+                name: 'host',
+                required: true
+            },
+            {
+                description: 'Slack login email',
+                name: 'email',
+                required: true
+            },
+            {
+                description: 'Slack password',
+                name: 'password',
+                hidden: true,
+                required: true
+            }
+        ],
+            (err, result) => err ? promiseReject(err) : promiseResolve(result),
+        )
+    });
+}
+
+/**
+ * 
+ */
+async function login(page: Page, userInput: UserInput): Promise<void> {
+    console.log('userInput', userInput);
+    await page.goto(ENTRY_URL_FACTORY(userInput.host));
+
+    const emailInputSelector = '#signin_form input[type=email]';
+    await page.waitForSelector(emailInputSelector, { visible: true });
+    setInputElementValue(page, emailInputSelector, userInput.email);
+
+    setInputElementValue(page, '#signin_form input[type=password]', userInput.password);
+
+    const signinButtonElement = await page.$('#signin_form #signin_btn');
+    await signinButtonElement.click();
+
+    await page.waitForSelector(emailInputSelector, { hidden: true });
+
+}
+
+/**
+ * 
+ */
+function loadEmojiPack(path: string): Promise<EmojiPack> {
+    return new Promise((promiseResolve, promiseReject) => {
+        const yamlPath = resolve(__dirname, 'emoji', path);
+        readFile(yamlPath, (error, yamlContent) => {
+            if (error) {
+                promiseReject(new Error('Unable to read emoji pack.'));
+            }
+            promiseResolve(yaml.load(yamlContent.toString()));
+        });
+    });
+}
+
+/**
+ * 
+ */
+function downloadImage(url: string): Promise<string> {
+
+    return new Promise((promiseResolve, promiseReject) => {
+
+        if (!/^https?:\/\//.test(url)) {
+            promiseReject(new Error(`Invalid url ${url}`));
+        }
+
+        const target = resolve(TEMP_DIR, basename(url));
+        request(url).pipe(createWriteStream(target)).on('finish', () => promiseResolve(target));
+
+    });
+
+}
+
+/**
+ * 
+ */
+async function upload(page: Page, imagePath: string, name: string): Promise<void> {
+
+    const addEmojiButtonElement = await page.waitForSelector('.p-customize_emoji_wrapper .c-button--primary', { visible: true });
+    await addEmojiButtonElement.click();
+
+    const fileInputElement = await page.waitForSelector('input#emojiimg');
+    await fileInputElement.uploadFile(imagePath);
+
+    const nameInputElement = await page.$('#emojiname');
+    await nameInputElement.type(name);
+
+    const saveEmojiButtonElement = await page.$('.c-dialog__footer .c-button--primary');
+    await saveEmojiButtonElement.click();
+
+    await page.waitForSelector('.c-dialog__footer .c-button--primary', { hidden: true });
+}
+
+/**
+ * 
+ */
+async function setInputElementValue(page: Page, querySelector: string, value: string) {
+    await page.waitForSelector(querySelector);
+    return page.$$eval(querySelector, (element: HTMLInputElement[], v) => element[0].value = v, value);
+}
+
+/**
+ * Adds delay to promise chain
+ */
+function sleep(time: number): () => Promise<void> {
+    return () => new Promise(resolve => setTimeout(() => resolve(), time));
+}
---
+++ readme.md
@@ -0,0 +1,44 @@
+# Slack Emoji Import
+
+A slack builk emoji importer built on [puppeteer](https://pptr.dev/) heavily based on [emojipacks](https://github.com/lambtron/emojipacks).
+
+## Usage
+
+* `npm i -g slack-emoji-import`
+* `slack-emoji-import path/to/emoji-pack.yaml`
+
+## Emoji Packs
+
+You can find emoji packs here: [https://emojipacks.com/](https://emojipacks.com/)
+
+- [animals](https://raw.githubusercontent.com/lambtron/emojipacks/master/packs/animals.yaml)
+- [clippy](https://raw.githubusercontent.com/lambtron/emojipacks/master/packs/clippy.yaml)
+- [fika](https://raw.githubusercontent.com/lambtron/emojipacks/master/packs/fika.yaml)
+- [frontend](https://raw.githubusercontent.com/lambtron/emojipacks/master/packs/frontend.yaml)
+- [harrypotter](https://raw.githubusercontent.com/lambtron/emojipacks/master/packs/harrypotterhouses.yaml)
+- [mario](https://raw.githubusercontent.com/lambtron/emojipacks/master/packs/mario-8bit.yaml)
+- [occupy](https://raw.githubusercontent.com/lambtron/emojipacks/master/packs/occupy.yaml)
+- [officespace](https://raw.githubusercontent.com/lambtron/emojipacks/master/packs/officespace.yaml)
+- [omnom](https://raw.githubusercontent.com/lambtron/emojipacks/master/packs/omnom.yaml)
+- [futurama](https://raw.githubusercontent.com/lambtron/emojipacks/master/packs/futurama.yaml)
+- [food](https://raw.githubusercontent.com/lambtron/emojipacks/master/packs/food.yaml)
+- [skype](https://raw.githubusercontent.com/lambtron/emojipacks/master/packs/skype.yaml)
+- [starwars](https://raw.githubusercontent.com/lambtron/emojipacks/master/packs/starwars.yaml)
+- [startups](https://raw.githubusercontent.com/lambtron/emojipacks/master/packs/startups.yaml)
+- [businessfish](https://raw.githubusercontent.com/lambtron/emojipacks/master/packs/businessfish.yaml)
+- [hipchat](https://raw.githubusercontent.com/lambtron/emojipacks/master/packs/hipchat.yaml)
+- [twitch](https://raw.githubusercontent.com/lambtron/emojipacks/master/packs/twitch.yaml)
+- [parrotparty](https://raw.githubusercontent.com/lambtron/emojipacks/master/packs/parrotparty.yaml) ([Parrot](http://cultofthepartyparrot.com/) [Paint](http://cultofthepartyparrot.com/paint/))
+- [Finland](https://raw.githubusercontent.com/lambtron/emojipacks/master/packs/finland.yaml)
+- [pokemongo: items](https://raw.githubusercontent.com/lambtron/emojipacks/master/packs/pokemongo.yaml)
+- [Pokémon Go: Pokémon](https://raw.githubusercontent.com/Templarian/slack-emoji-pokemon/master/pokemon.yaml) ([Prefixed `pokemon-*`](https://raw.githubusercontent.com/Templarian/slack-emoji-pokemon/master/pokemon-prefix.yaml))
+- [politipack](https://raw.githubusercontent.com/lambtron/emojipacks/master/packs/politipack.yaml)
+- [nekoatsume](https://raw.githubusercontent.com/lambtron/emojipacks/master/packs/nekoatsume.yaml)
+- [octicons](https://raw.githubusercontent.com/lambtron/emojipacks/master/packs/octicons.yaml)
+- [pokemon](https://raw.githubusercontent.com/jaylynch/pokemoji/master/pokemon-by-name.yaml)
+- [devicon](https://raw.githubusercontent.com/izumin5210/emojipack-for-devicon/master/png/devicon.yaml) ([Devicon](http://devicon.fr/))
+- [hamsterdance](https://raw.githubusercontent.com/snipe/hamsterdance-emojipack/master/hamsterdance.yaml) ([snipe/emojipacks](https://github.com/snipe/hamsterdance-emojipack))
+- [avengers](https://raw.githubusercontent.com/lambtron/emojipacks/master/packs/avengers.yaml)
+- [Shiba Stickers](https://raw.githubusercontent.com/lambtron/emojipacks/master/packs/shiba.yaml) (from Messenger)
+- [gamedevmoji](https://raw.githubusercontent.com/niksudan/gamedevmoji/master/gamedevicons.yaml)
+- [AWS simple icons](https://raw.githubusercontent.com/Surgo/aws_emojipacks/master/noprefix-emojipacks.yml)
